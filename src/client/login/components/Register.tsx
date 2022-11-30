96330f46832b62a30925a22559913a85db7cd9c4
unknown <nam664221998@gmail.com>
2022-11-30 22:07:04 +0700

    update/ui_login

--- src/client/login/components/Register.tsx
+++ src/client/login/components/Register.tsx
@@ -1,24 +1,5 @@
 import { Link } from 'react-router-dom'
-import { ToastContainer } from 'react-toastify'
 
 export const Register = () => {
-  return (
-    <div>
-      <form>
-        <div>
-          Email
-          <input type="email" name="email" placeholder="Email" />
-        </div>
-        <div>
-          Pass
-          <input type="password" name="email" placeholder="Password" />
-        </div>
-        <button type="submit">Submit</button>
-        <span>
-          Already have an account ? <Link to="/login"> Login </Link>
-        </span>
-      </form>
-      <ToastContainer />
-    </div>
-  )
+  return <div></div>
 }
